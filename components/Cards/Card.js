85042cfdbe89d8ecbfe116652877fa8fc23b0c95
kevduong <duong.kev@gmail.com>
2019-08-13 14:48:27 -0700

    Added Input component

--- components/Cards/Card.js
+++ components/Cards/Card.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StyledCard } from './style';
 import DateTitle from '../elements/DateTitle';
+import moment from 'moment';
+import InputTask from '../elements/InputTask';
 
 export default function Card() {
   const date = {
-    title: 'Hello World'
+    title: moment().format("dddd, MMMM Do YYYY")
   }
 
   return (
     <StyledCard>
-      <DateTitle date={date}/>
+      <DateTitle date={date} />
+      <InputTask />
     </StyledCard>
   );
 };
---
+++ components/elements/InputTask.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from 'semantic-ui-react'
+
+const InputTask = () => {
+  return (
+    <Input action='Add' placeholder='Enter a task...' />
+  )
+}
+
+export default InputTask;
