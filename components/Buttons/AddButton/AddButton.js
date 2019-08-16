1b5e2a5762da8f4d9f5df05be46b7253cc9e47fc
kevduong <duong.kev@gmail.com>
2019-08-16 15:32:32 -0700

    wip handleSubmit

--- components/Buttons/AddButton/AddButton.js
+++ components/Buttons/AddButton/AddButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Icon, Input } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
 const btnStyle = {
   position: 'absolute',
@@ -9,64 +9,22 @@ const btnStyle = {
 }
 
 class AddButton extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      inputStatus: "inputHide"
-    };
-
-    this.inputClose = this.inputClose.bind(this);
-    this.inputOpen = this.inputOpen.bind(this);
-  }
-
-  inputClose(e) {
-    e.stopPropagation();
-    this.setState({
-      inputStatus: "inputHide",
-      fade: true
-    });
-  }
-
-  inputOpen() {
-    this.setState({
-      inputStatus: "inputShow"
-    })
-  }
-
   render() {
-    const fade = this.state.fade
-
     return (
       <div>
-        {this.state.inputStatus == 'inputHide' ?
-          <Button
-            animated
-            color="teal"
-            style={btnStyle}
-            onClick={this.inputOpen}
-          >
-            <Button.Content visible>Add Task</Button.Content>
-            <Button.Content hidden className={this.state.inputStatus}>
-              <Icon name='plus circle' color="white" />
-            </Button.Content>
-          </Button> :
-          null
-        }
-        {this.state.inputStatus == 'inputShow' ?
-          <Input
-            className="inputClose"
-            style={btnStyle}
-            action={{
-              color: 'teal',
-              icon: 'plus circle'
-            }}
-          /> :
-          null
-        }
+        <Button
+          animated
+          color="teal"
+          style={btnStyle}
+        >
+          <Button.Content visible>Add Task</Button.Content>
+          <Button.Content hidden>
+            <Icon name='plus circle' />
+          </Button.Content>
+        </Button>
       </div>
     )
   }
 }
 
-export default AddButton;
+export default AddButton
--- components/Cards/Card.js
+++ components/Cards/Card.js
@@ -4,17 +4,80 @@ import { StyledCard } from './style';
 import DateTitle from '../elements/DateTitle';
 import TaskContainer from '../elements/TaskContainer';
 import AddButton from '../Buttons/AddButton/AddButton';
+import { Input } from 'semantic-ui-react';
 
-export default function Card() {
-  const date = {
-    title: moment().format("dddd, MMMM Do YYYY")
+class Card extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isHidden: true,
+      items: []
+    };
+
+    this.showInput = this.showInput.bind(this);
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  return (
-    <StyledCard>
-      <DateTitle date={date} />
-      <TaskContainer/>
-      <AddButton/>
-    </StyledCard>
-  );
-};
+  showInput(e) {
+    e.preventDefault();
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  }
+
+  updateInputValue(e) {
+    e.preventDefault();
+    this.setState({
+      items: [
+        e.target.value
+      ]
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      items: [
+        e.target.value
+      ]
+    });
+  }
+
+  render() {
+    const date = {
+      title: moment().format("dddd, MMMM Do YYYY")
+    }
+
+    return (
+      <StyledCard
+        addItem={this.addItem}
+        inputElement={this.inputElement}
+        handleInput={this.handleInput}
+        currentItem={this.currentItem}
+      >
+        <DateTitle date={date} />
+        <TaskContainer />
+        <div onClick={this.showInput}>
+          {this.state.isHidden && <AddButton />}
+        </div>
+        {!this.state.isHidden ?
+          <form onSubmit={this.handleSubmit}>
+            <Input
+              className="inputClose"
+              action='submit'
+              color="teal"
+              ref={this.props.inputElement}
+              value={this.state.items}
+              onChange={this.handleSubmit}
+            />
+          </form> :
+          null
+        }
+      </StyledCard>
+    );
+  };
+}
+
+export default Card;
